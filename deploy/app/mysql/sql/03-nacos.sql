3f913e74011c245bc6e1eb8303f8e72c8c9e1e74
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-21 19:59:03 +0800

    continuing

--- deploy/app/mysql/sql/03-nacos.sql
+++ deploy/app/mysql/sql/03-nacos.sql
@@ -229,7 +229,7 @@ CREATE TABLE `permissions`
 );
 
 INSERT INTO users (username, password, enabled)
-VALUES ('nacos', '$2a$10$9IcbyRmE9Tulsa1baGrdNeQ4YiHiU9mRgyhuB.LwnntSb9JVPKLSi', TRUE);
+VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuSwc2TlQxOqnJ5Ki9Kp', TRUE);
 
 INSERT INTO roles (username, role)
 VALUES ('nacos', 'ROLE_ADMIN');
