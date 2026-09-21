89242d589f035e7d464f5fe3473c8f8c4fb51f9f
wjlwjlwjlwjl-cmd <1393265226@qq.com>
2026-09-21 13:58:18 +0000

    nacos 部署成功，解决初始账号、密码问题

--- deploy/app/mysql/sql/03-nacos.sql
+++ deploy/app/mysql/sql/03-nacos.sql
@@ -229,7 +229,7 @@ CREATE TABLE `permissions`
 );
 
 INSERT INTO users (username, password, enabled)
-VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuSwc2TlQxOqnJ5Ki9Kp', TRUE);
+VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
 
 INSERT INTO roles (username, role)
 VALUES ('nacos', 'ROLE_ADMIN');
